1fa1e50fb32e98d0e5c2455654f37939dbfaa25f
hss01248 <hss012489@gmail.com>
2025-11-12 11:36:52 +0800

    feat(google-play): 添加强制检查更新功能
    - 在GooglePlayUpdater中启用强制更新检查选项
    - 修改checkUpdate方法调用以传递强制参数- 更新主活动中的按钮点击处理逻辑

--- app/src/main/java/com/hss01248/google_play_functions/MainActivity.java
+++ app/src/main/java/com/hss01248/google_play_functions/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void checkUpdate(View view) {
-        GooglePlayUpdater.checkUpdate();
+        GooglePlayUpdater.checkUpdate(true);
 
     }
 }
